aa3a34974351aa1b7752f82ae7262243c6eb2bdc
Lloyd Grubham <grubham@gmail.com>
2017-08-04 18:57:12 -0600

    Added more commands.

--- app/mainCtrl.js
+++ app/mainCtrl.js
@@ -12,13 +12,20 @@ angular.module('app').controller('mainCtrl', function($scope, $interval, $anchor
         }
         return nums.reverse().join('.')
     }
+    $scope.test = 'Green Hat Console';
+    $scope.sip = '192.168.0.1';
+    $scope.cip = $scope.sip;
+    $scope.arr = [];
+    $scope.input;
+    $scope.pastCmd = [];
+    $scope.futureCmd = [];
 
-    function parseDir(dir) {
-        return dir
-    }
-    function parseCommand(cmd) {
-        return cmd
-    }
+    // function parseDir(dir) {
+    //     return dir
+    // }
+    // function parseCommand(cmd) {
+    //     return cmd
+    // }
 
     function hack(){
         $interval($scope.interval,300)
@@ -27,22 +34,53 @@ angular.module('app').controller('mainCtrl', function($scope, $interval, $anchor
          $scope.arr = [];
     }
     function help() {
-         $scope.arr.push(Object.keys($scope.commands))
+         $scope.arr.push('Availible Commands: '+Object.keys($scope.commands).join(' | '))
     }
     function pwd() {
-         $scope.arr.push($scope.curDir);        
+         $scope.arr.push('/'+$scope.curDir);        
     }
+    function getLS(arr, obj){
+        if (arr.length == 1){
+            return obj[arr[0]]
+        } else {
+            let obj2 = obj[arr[0]];
+            let arr2 = arr.slice(1);
+            console.log(arr2)
+            console.log(obj2)
+            return getLS(arr2, obj2)
+        }
+    }
+
     function ls() {
-        $scope.arr.push(Object.keys($scope.fs['root']));         
+        let arr = $scope.curDir.split('/');
+        let fs = $scope.fs;
+        
+        let x = getLS(arr, fs);
+        $scope.arr.push(Object.keys(x).join(' | '));         
     }
+
     function cd(newDir) {
-        
+        let dir = getLS($scope.curDir.split('/'),$scope.fs)
+        if (newDir[0] == '..' && $scope.curDir != 'root'){
+            let fd = $scope.curDir.split('/');
+            fd.pop()
+            $scope.curDir = fd.join('/');
+        } else if (dir.hasOwnProperty(newDir[0])){
+            $scope.curDir +=('/'+  newDir[0]);
+        } else if (!newDir[0]) {
+            $scope.curDir = 'root'
+        } else {
+            $scope.arr.push("Invalid directory");
+        }
     }
-    function mkdir() {
-        
+    function mkdir(newDir) {
+        let dir = getLS($scope.curDir.split('/'),$scope.fs);
+        dir[newDir[0]] = {}
+
     }
-    function touch() {
-        
+    function touch(newFile) {
+        let dir = getLS($scope.curDir.split('/'),$scope.fs);
+        dir[newFile[0]] = []
     }
     
     $scope.commands = {
@@ -50,49 +88,61 @@ angular.module('app').controller('mainCtrl', function($scope, $interval, $anchor
         clear: clear,
         help: help,
         pwd: pwd,
-        ls: ls
+        ls: ls,
+        cd: cd,
+        mkdir: mkdir,
+        touch: touch
     }
 
-    $scope.test = 'Green Hat Console'
-    $scope.sip = '192.168.0.1'
-    $scope.cip = $scope.sip
-    $scope.arr = []
-    $scope.input;
-
     $scope.cmd = function () {
-        let comm = $scope.input.trim()
-        if ($scope.commands.hasOwnProperty(comm)){
-            $scope.arr.push('./ '+comm);
-            $scope.commands[$scope.input]();
+        let comm = $scope.input.split(' ')
+        if ($scope.commands.hasOwnProperty(comm[0])){
+            $scope.arr.push('./ '+$scope.input);
+            $scope.commands[comm[0]](comm.slice(1));
 
         } else {
             $scope.arr.push('./'+$scope.input);
             $scope.arr.push('./'+$scope.input + ' is not a valid command.');            
         }
         $scope.input = '';
+        scrollToBottom();        
     }
+    $scope.curDir = 'root/documents'
     $scope.fs = {
         root: {
             documents: {
                 pictures: {
+                    '2016':{
 
+                    },
+                    '2017': {
+
+                    },
+                    funny: {
+
+                    }
                 }
             },
             user: {
 
+            },
+            network: {
+
             }
         } 
     }
-    $scope.curDir = '/root'
 
     $scope.clear = function () {
          $scope.arr = [];
     }
 
     function scrollToBottom() {
-        $location.hash("form");
+        $location.hash("scrollHere");
         $anchorScroll()
     }
+    // $scope.catchKP = function () {
+    //     console.log($event)
+    // }
     $scope.interval = function(){
         $scope.arr.push(`->Scanning ip address: ${$scope.cip}`);
         $scope.cip = incIP($scope.cip);
